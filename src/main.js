4e08d33a6a08b83f2b01ee4923670e3fc217718e
nitunwa <nitun.2@gmail.com>
2020-11-15 23:12:11 -1000

    add dist file

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@ import moment from 'moment-timezone';
 
 const time_zone = document.getElementById('timezone').value;
 const currentSubcsriberTimeZone = dateOfDay(time_zone);
+console.log("currentSubcsriberTimeZone");
+console.log(currentSubcsriberTimeZone);
+let element = document.getElementById('schedule');
+let scheduleUI = '';
+
+currentSubcsriberTimeZone.forEach(function (value, key) {
+  scheduleUI += `<div class="float-child">
+  <h5>${key}:</h5>${value}
+</div>`
+  //console.log(`${key}: ${value}<br>`)
+
+});
+element.innerHTML = scheduleUI;
 
 
 
-const element = document.getElementById('output');
-element.innerHTML = `<div>
-<h5></h5>
-<h5></h5>
-</div>`;
+// const element = document.getElementById('output');
+
 
 
 
@@ -28,7 +38,9 @@ function dateOfDay(timezone) {
   console.log("   contributor_schedule   " + contributor_schedule);
   let dayName = '';
   let dayINeed = 0;
-  let browser_timezone = '';
+  let browser_time_zone = '';
+
+  let map = new Map();
   const utc = contributor_schedule[0];
   const new_utc = utc.substring(3, utc.length).replaceAll(" ", "");
   const currentUtc = (new_utc.length !== 3) ? new_utc[0] + "0" + new_utc[1] : new_utc;
@@ -37,47 +49,48 @@ function dateOfDay(timezone) {
 
     if (contributor_schedule[i].trim() !== '') {
       let clock = contributor_schedule[i].split(' ');
-
-      //if (clock[0].trim() !== 'UTC') {
-
       dayName = clock[0];
       console.log("Day: " + dayName);
       dayINeed = dayMapping[dayName];
       console.log("weekday:" + dayMapping[dayName]);
       const dateOfday = getDateOfDay(dayINeed);
       console.log("  date Of day   " + dateOfday);
-      browser_timezone = convertToBroserTime(dateOfday, clock[1]);
-      //}
+      const clock1 = clock[1].split(',');
+      console.log('clock:' + clock1);
+
+      for (let i = 0; i < clock1.length; i++) {
+        browser_time_zone = convertToBroserTime(dateOfday, clock1[i], currentUtc);
+        console.log('browser_timezone ' + browser_time_zone);
+        let browser_day_name = moment(browser_time_zone).format('dddd');
+        console.log(' browser day ' + browser_day_name);
+        let browser_day_time = moment(browser_time_zone).format('hh:mm:ss A');
+        console.log(' browser_day_time ' + browser_day_time);
+
+        if (map.has(browser_day_name)) {
+          console.log("map " + map.get(browser_day_name))
+          map.set(browser_day_name, map.get(browser_day_name) + "," + browser_day_time);
+        } else {
+          map.set(browser_day_name, browser_day_time);
+        }
+
+        //console.log(map);
+      }
     }
   }
-
+  return map;
 }
 
 /* browser */
 
-function convertToBroserTime(dateOfday, clock) {
+function convertToBroserTime(dateOfday, clock, currentUtc) {
   let map = new Map();
-  // for(let i=0;i<clock.length;i++){
 
-  // }
-  const channelTime = moment(dateOfday + "T12:10:00+6:00", "YYYY-MM-DDThh:mm:ssZ");
+  const channelTime = moment(dateOfday + "T" + clock + currentUtc + ":00", "YYYY-MM-DDThh:mm:ssZ");
+  // const channelTime = moment(dateOfday + "T12:10:00+6:00", "YYYY-MM-DDThh:mm:ssZ");
   console.log("timezone::::::: " + moment.tz.guess());
   const browserTimeZone = moment.tz.guess();
   const browser_time_zone = channelTime.tz(browserTimeZone).format();
   console.log("date formatted: " + channelTime.tz(browserTimeZone).format('MM-DD-YYYYThh:mm:ssZ'));
-
-  //var myDateVariable= moment("01/01/2019").format("dddd Do MMMM YYYY")
-  // get the day. =>Friday 
-  let browser_day_name = moment(browser_time_zone).format('dddd');
-  console.log(' browser day ' + browser_day_name);
-  let browser_day_time = moment(browser_time_zone).format('hh:mm:ss');
-  console.log(' browser_day_time ' + browser_day_time);
-  if (map.has(browser_day_name)) {
-    map.set(browser_day_name, browser_day_time)
-  } else {
-    map.set(browser_day_name, browser_day_time)
-  }
-
   return browser_time_zone;
 }
 
